9cfe4c79b50c8f86c6aaeb1ccf218b0d70814d19
NguyenVanDuongTDH <154052803+NguyenVanDuongTDH@users.noreply.github.com>
2024-03-01 20:06:02 +0700

    op

--- example/snap7_example.dart
+++ example/snap7_example.dart
@@ -1,5 +1,20 @@
-import 'package:snap7/snap7.dart';
+import 'dart:ffi';
 
-void main() {
-  S7Client client = S7Client();
+import 'package:ffi/ffi.dart';
+import 'package:snap7/snap7.dart';
+import 'package:snap7/src/s7_server.dart';
+
+Future<void> main() async {
+    S7Server s7server = S7Server();
+  int size = 100;
+  Pointer<Uint8> data = calloc.allocate(size);
+  s7server.registerArea(5, 1, data, size);
+  s7server.startTo("192.168.1.6" ,port: 1402);
+  while (true) {
+    final e = s7server.getEvent();
+    if (e != null) {
+      print(e);
+    }
+    await Future.delayed(Duration(milliseconds: 50));
+  }
 }
--- test/snap7_test.dart
+++ test/snap7_test.dart
@@ -9,21 +9,15 @@ import 'db.dart';
 
 Future<void> main() async {
   S7Client client = S7Client();
-  int begin = DateTime.now().millisecondsSinceEpoch;
-  bool c = client.ConnectTo("192.168.1.6", 0, 1, port: 5505);
-  var fina = DateTime.now().millisecondsSinceEpoch - begin;
-  print(fina);
-   begin = DateTime.now().millisecondsSinceEpoch;
+  bool c = client.ConnectTo("127.0.0.1", 0, 1, port: 5505);
   print(client.DBRead(1, 0, 10));
-     fina = DateTime.now().millisecondsSinceEpoch - begin;
-  print(fina);
   client.Disconnect();
 
   // S7Server s7server = S7Server();
   // int size = 100;
   // Pointer<Uint8> data = calloc.allocate(size);
   // s7server.registerArea(5, 1, data, size);
-  // s7server.startTo("192.168.1.6" ,port: 1402);
+  // s7server.startTo("0.0.0.0" ,port: 5505);
   // while (true) {
   //   final e = s7server.getEvent();
   //   if (e != null) {
